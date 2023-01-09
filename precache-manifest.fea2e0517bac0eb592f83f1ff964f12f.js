19fbf5fb91a23787d971e61de0245d1621bc015a
Keshav Gaur <keshav Gaur>
2023-01-09 13:36:56 +0530

    Updates

---
+++ precache-manifest.fea2e0517bac0eb592f83f1ff964f12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55d030234ea78b7e5aa9",
+    "url": "/MY-PROJECTS/static/css/main.d65458d9.chunk.css"
+  },
+  {
+    "revision": "55d030234ea78b7e5aa9",
+    "url": "/MY-PROJECTS/static/js/main.55d03023.chunk.js"
+  },
+  {
+    "revision": "0765819dcae452713017",
+    "url": "/MY-PROJECTS/static/js/1.0765819d.chunk.js"
+  },
+  {
+    "revision": "4407399a36e11084c4bc",
+    "url": "/MY-PROJECTS/static/js/2.4407399a.chunk.js"
+  },
+  {
+    "revision": "473b2c516a3dd80ceba5",
+    "url": "/MY-PROJECTS/static/js/runtime~main.473b2c51.js"
+  },
+  {
+    "revision": "d97d57d05a655e4c61017455059d934f",
+    "url": "/MY-PROJECTS/index.html"
+  }
+];
